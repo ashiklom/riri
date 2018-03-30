21332a00cdc8584e75fa85c227d5319414a45e47
Alexey Shiklomanov <ashiklom@bu.edu>
2018-03-30 15:06:51 -0400

    Segment large site groups into smaller pieces
    Closes #1

--- R/get_tabular.R
+++ R/get_tabular.R
@@ -3,18 +3,50 @@
 #' @param source_string An expert mode string or character vector
 #' @param sites_df A data frame of site information. Must contain columns 
 #' `site_name`, `latitude`, and `longitude`
+#' @param group_size Size of groups for segmenting data. Default = 20.
 #' @return `tibble` containing data to be returned
 #' @export
-get_tabular <- function(source_string, sites_df, ...) {
+get_tabular <- function(source_string, sites_df, group_size = 20, verbose = TRUE, ...) {
   src <- paste(source_string, collapse = " ")
+  nsites <- nrow(sites_df)
+  if (nsites > group_size) {
+    if (verbose) {
+      message(
+        "Number of sites (", nsites, ") ",
+        "greater than group size (", group_size, "). ",
+        "Segmenting input dataframe."
+      )
+    } 
+    get_tabular_pb <- function(sites_sub, pb) {
+      get_tabular(
+        source_string = source_string,
+        sites_df = sites_sub,
+        group_size = group_size,
+        verbose = verbose,
+        ...
+      )
+    }
+    sites_segmented <- segment_df(sites_df, group_size)
+    pb <- progress::progress_bar$new(total = nrow(sites_segmented))
+    pb$tick(0)
+    met_data <- sites_segmented %>%
+      dplyr::mutate(
+        .riri_data = purrr::map(.segmented_data, get_tabular_pb, pb = pb)
+      )
+    result <- met_data %>%
+      dplyr::pull(.riri_data) %>%
+      purrr::reduce(dplyr::full_join, by = "T")
+    return(result)
+  }
   site_expert <- site2expert(sites_df)
   nsite <- nrow(sites_df)
   all_expert <- expert2string(paste(src, site_expert))
   src_url <- expert2url(all_expert, "")
   file_url <- paste(src_url, ncoltable(nsite), sep = "/")
-  message("Downloading data...")
-  dat <- readr::read_tsv(file_url)
-  message("Done!")
+  col_spec <- readr::cols(T = "c", .default = "d")
+  if (verbose) message("Downloading data...")
+  dat <- readr::read_tsv(file_url, col_types = col_spec)
+  if (verbose) message("Done!")
   colnames(dat)[-1] <- sites_df$site_name
   dat
 }
@@ -50,4 +82,11 @@ site2expert <- function(sites_df, xvar = ".T") {
 
 latlon2string <- function(latitude, longitude) {
   sprintf("X (%f) VALUE Y (%f) VALUE", longitude, latitude)
+}
+
+segment_df <- function(.data, group_size = 5) {
+  .data %>%
+    dplyr::mutate(.segment_group = 1 + (row_number() - 1) %/% !!group_size) %>%
+    dplyr::group_by(.segment_group) %>%
+    tidyr::nest(.key = ".segmented_data")
 }
--- tests/testthat/test.multi_site.R
+++ tests/testthat/test.multi_site.R
@@ -7,7 +7,9 @@ site_df <- tibble::tribble(
   "Harvard Forest", 42.5, -72.2,
   "UNDERC", 46.2, -89.5,
   "Honolulu", 21.3, -157.8
-)
+) %>%
+  dplyr::bind_rows(., ., ., ., ., ., ., ., ., ., ., .) %>%
+  dplyr::mutate(site_name = paste0("site_", row_number()))
 
 temp_src <- c("SOURCES .NOAA .NCEP .CPC .GHCN_CAMS .gridded .deg0p5 .temp")
 temp_data <- get_tabular(temp_src, site_df)
